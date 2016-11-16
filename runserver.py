f870934c46e28a1f0e0771edbda8eb82dda9aaa7
Filter <IamFilter94@gmail.com>
2016-11-17 02:06:28 +0600

    Testing new internal ip.

--- runserver.py
+++ runserver.py
@@ -14,10 +14,10 @@ api_token_file.close()
 
 WEBHOOK_HOST = 'g'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+WEBHOOK_LISTEN = '10.0.0.4'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = 'webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
@@ -71,6 +71,7 @@ def echo_message(message):
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
 bot.remove_webhook()
+
 
 # Set webhook
 bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
