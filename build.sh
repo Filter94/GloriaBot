262ed6a43ac3aa4522e62adf0aa34a7aa2afe4ec
Filter <IamFilter94@gmail.com>
2016-11-17 01:31:01 +0600

    Testing build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+git pull origin master
+. telegram_bot/bin/activate
 pip install -r requirements.txt
+python runserver.py
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh Roman@13.82.139.124 '. ~/UleyChatGloria/GloriaBot/build.sh'
