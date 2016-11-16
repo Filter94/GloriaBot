b853aa2fdd305f7f1fca59fca139b495f1b8786e
Filter <IamFilter94@gmail.com>
2016-11-17 03:18:42 +0600

    Uncommented webhooks setting.

--- runserver.py
+++ runserver.py
@@ -71,13 +71,13 @@ def echo_message(message):
 
 bot.get_webhook_info()
 
-# # Remove webhook, it fails sometimes the set if there is a previous webhook
-# bot.remove_webhook()
-#
-#
-# # Set webhook
-# bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
-#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Start flask server
 app.run(host=WEBHOOK_LISTEN,
