585180b9f9540797c783ad8d6996d63ed5982399
Filter <IamFilter94@gmail.com>
2016-11-17 21:18:10 +0600

    Trying to get it work [2].

--- runserver.py
+++ runserver.py
@@ -75,9 +75,9 @@ bot.remove_webhook()
 # Set webhook
 bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH,
                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# Start flask server
-app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-        debug=True)
+if __name__ == '__main__':
+    # Start flask server
+    app.run(host=WEBHOOK_LISTEN,
+            port=WEBHOOK_PORT,
+            ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
+            debug=True)
