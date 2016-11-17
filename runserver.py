2fe14008ca83541bface3bb5788d45d74181b606
Filter <IamFilter94@gmail.com>
2016-11-17 21:15:54 +0600

    Spam mode off

--- runserver.py
+++ runserver.py
@@ -64,7 +64,7 @@ def send_welcome(message):
 
 
 # Handle all other messages
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+@bot.message_handler(func=lambda message: False, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
 
