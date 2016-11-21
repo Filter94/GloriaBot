3c7bde9b3f38eeb299b4f2c16c70037bda2d2f98
Roman <IamFilter94@gmail.com>
2016-11-21 23:20:14 +0600

    Fixed auto refactoring bugs.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-. ../telegram_bot/bin/activate
+. telegram_bot/bin/activate
 gunicorn -b 127.0.0.1:5000 --reload --log-file gunicorn.log -p pid -w 1 gloria_bot.runserver:app -D
 deactivate
