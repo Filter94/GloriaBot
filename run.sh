d29022e42e6fb4a6b0ed78d83ad271d75c9d4201
Roman <IamFilter94@gmail.com>
2016-11-21 23:09:47 +0600

    Changed log file. Updated gitignore.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 . telegram_bot/bin/activate
-gunicorn -b 127.0.0.1:5000 --reload --log-file gunicorn.txt -p pid -w 1 runserver:app -D
+gunicorn -b 127.0.0.1:5000 --reload --log-file gunicorn.log -p pid -w 1 runserver:app -D
