e17e41d48deed02fd1257d666af8674099741ac2
Roman <IamFilter94@gmail.com>
2016-11-25 11:54:59 +0600

    Replaced few handlers.

--- gloria_bot/handlers_init.py
+++ gloria_bot/handlers_init.py
@@ -3,6 +3,7 @@
 # add all handlers
 import logging
 from telegram.ext import CommandHandler
+from telegram.ext import RegexHandler
 
 from gloria_bot import callbacks
 from gloria_bot.handlers.regex_probability_handler import RegexProbabilityHandler
@@ -29,25 +30,25 @@ def init_handlers():
     dp.add_handler(RegexProbabilityHandler(ur'(?iu).*(тема|тёма|артем).*', callbacks.tema_pidor, 0.5))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu).*(гева|геворг|геворк).*', callbacks.geva_pidor, 0.5))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu).*(стас|пидорас|пидор).*', callbacks.stas_pidor, 0.5))
-    dp.add_handler(RegexProbabilityHandler(ur'debug', callbacks.debug))
+    dp.add_handler(RegexHandler(ur'debug', callbacks.debug))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu).*(на ?хуй|в пизду|впизду),? глория.*', callbacks.ban, 0.9))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu)(.{0,6}|добр{0,6})\s?(прив|утр[оа]).{0,20}', callbacks.morning, 0.9))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu)(.{0,10}|добр{0,10})\s?ночи.{0,20}', callbacks.night, 0.9))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu).*(трахать|ебать|секс).*', callbacks.sex, 0.9))
     dp.add_handler(RegexProbabilityHandler(ur'(?iu).*(%s).*' % eat_choice,
                                            callbacks.eat, 0.9))
-    dp.add_handler(RegexProbabilityHandler(
+    dp.add_handler(RegexHandler(
         ur'(?iu).*глория,? покажи (?P<video_type>видос|видео|ролик) про ?(?P<keyword>.*)',
-        callbacks.search_video_about, 1, pass_groupdict=True))
-    dp.add_handler(RegexProbabilityHandler(
+        callbacks.search_video_about, pass_groupdict=True))
+    dp.add_handler(RegexHandler(
         ur'(?iu).*глория,? покажи (?P<video_type>видос|видео|ролик) ?"?(?P<keyword>.*)"?',
-        callbacks.search_video, 1, pass_groupdict=True))
-    dp.add_handler(RegexProbabilityHandler(
+        callbacks.search_video, pass_groupdict=True))
+    dp.add_handler(RegexHandler(
         ur'(?iu).*глория,? покажи како[йе]-?нибудь( другой)? (?P<video_type>видос|видео|ролик) про ?(?P<keyword>.*)',
-        callbacks.search_some_video_about, 1, pass_groupdict=True))
-    dp.add_handler(RegexProbabilityHandler(
+        callbacks.search_some_video_about, pass_groupdict=True))
+    dp.add_handler(RegexHandler(
         ur'(?iu).*глория,? покажи како[йе]-нибудь( другой)? (?P<video_type>видос|видео|ролик) ?"?(?P<keyword>.*)"?',
-        callbacks.search_some_video, 1, pass_groupdict=True))
+        callbacks.search_some_video, pass_groupdict=True))
     # log all errors
     dp.add_error_handler(callbacks.error)
     logger.debug(u"Handlers size: %s" % len(dp.handlers[0]))
